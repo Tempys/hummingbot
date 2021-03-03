23340233b1dfcf91a7bd13f98d16f223694b6b5e
Tempys <ubuntu@DESKTOP-06RQAC0.localdomain>
2021-03-03 17:37:25 +0200

    add strategy template

--- hummingbot/strategy/cbs/start.py
+++ hummingbot/strategy/cbs/start.py
@@ -1,12 +1,12 @@
-from hummingbot.strategy.amm_arb.amm_arb_config_map import amm_arb_config_map
+from hummingbot.strategy.cbs.cbs_config_map import cbs_config_map
 from hummingbot.strategy.cbs.cbs import CbsStrategy
 from hummingbot.strategy.market_trading_pair_tuple import MarketTradingPairTuple
 
 
 def start(self):
-    connector_1 = amm_arb_config_map.get("connector_1").value.lower()
-    market_1 = amm_arb_config_map.get("market_1").value
-    order_amount = amm_arb_config_map.get("order_amount").value
+    connector_1 = cbs_config_map.get("connector_1").value.lower()
+    market_1 = cbs_config_map.get("market_1").value
+    order_amount = cbs_config_map.get("order_amount").value
 
     self._initialize_markets([(connector_1, [market_1])])
     base_1, quote_1 = market_1.split("-")
